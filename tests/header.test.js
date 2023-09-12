8c9354956deef6be1b80350ad490ed9ab1635f35
Ahmed Salem <as2720094@gmail.com>
2023-09-12 16:03:12 +0300

    Facking the login process then test that Logout button is appears on the page

--- tests/header.test.js
+++ tests/header.test.js
@@ -25,3 +25,31 @@ test("Clicking login", async () => {
   const url = await page.url();
   expect(url).toMatch(/accounts\.google\.com/);
 });
+
+/**
+ * Faking login to the application to be able to test all the functionality
+ * Testing logout button appearence
+ */
+
+test("logging in, and showing logout button", async () => {
+  id = "64eb74006e673c3a3c827ffa";
+  const Buffer = require("safe-buffer").Buffer;
+  const Keygrip = require("keygrip");
+  const sessionObj = {
+    passport: {
+      user: id,
+    },
+  };
+
+  sessionStr = Buffer.from(JSON.stringify(sessionObj)).toString("base64");
+  const keys = require("../config/keys");
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign("session=" + sessionStr);
+  await page.setCookie({ name: "session", value: sessionStr });
+  await page.setCookie({ name: "session.sig", value: sig });
+  await page.goto("localhost:3000");
+
+  const text = await page.$eval('a[href="/auth/logout"]', (el) => el.innerHTML);
+
+  expect(text).toEqual("Logout");
+});
