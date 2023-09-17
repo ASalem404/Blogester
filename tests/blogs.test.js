43ad7b75828d0b2bf04acfee3d11a04fa0fc2341
Ahmed Salem <as2720094@gmail.com>
2023-09-17 18:29:22 +0300

    Refactoring Unlogged user tests

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -57,27 +57,15 @@ describe("when logged in", async () => {
 
 describe("User is not logged in", () => {
   test("User cannot creating new blog", async () => {
-    const result = await page.evaluate(async () => {
-      return fetch("/api/blogs", {
-        method: "POST",
-        credentials: "same-origin",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          title: "Test Title",
-          content: "Test content",
-        }),
-      }).then((result) => result.json());
-    });
+    const blog = {
+      title: "Test Title",
+      content: "Test content",
+    };
+    const result = await page.post("/api/blogs", blog);
     expect(result).toEqual({ error: "You must log in!" });
   });
   test("User cannot viewing blogs", async () => {
-    const result = await page.evaluate(async () => {
-      return fetch("/api/blogs", {
-        method: "GET",
-        credentials: "same-origin",
-        headers: { "Content-Type": "application/json" },
-      }).then((res) => res.json());
-    });
+    const result = await page.get("/api/blogs");
     expect(result).toEqual({ error: "You must log in!" });
   });
 });
