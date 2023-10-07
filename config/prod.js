538b165e1e89ac6f7d070d92bf6ef3017407d5f8
Ahmed Salem <as2720094@gmail.com>
2023-10-07 23:57:57 +0300

    update the keys to have the s3 config variables

--- config/prod.js
+++ config/prod.js
@@ -3,4 +3,8 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
+  BucketUrl: process.env.BUCKET_URL,
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  bucketName: process.env.BUCKET_NAME,
 };
